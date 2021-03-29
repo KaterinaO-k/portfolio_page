cd6519dd627a51474671b372d427d9fe336cf3b7
KaterinaO-k <70755983+KaterinaO-k@users.noreply.github.com>
2021-03-29 23:06:25 +0300

    Add files via upload

---
+++ works/TEST_Everad/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.economy-offers__slider').slick({
+        dots: true
+    });
+});
+
+document.querySelector('.copyright').innerText = 'Copyright ' + new Date().getFullYear();
+
+
